f9636655e522840d272cd0e22aadfa3bf9c013f6
wangxian <xian366@126.com>
2016-06-28 09:17:37 +0800

    修改：测试where,set/data,query支持参数替换

--- demos/100.devtest/_app/controllers/dbController.php
+++ demos/100.devtest/_app/controllers/dbController.php
@@ -17,6 +17,36 @@ class dbController extends controller
 
 	}
 
+	public function testAction()
+	{
+		// $where = "id=? and name=?";
+		// $arr = [1, "2010-12-30"];
+		// $i = 0;
+		// dump(preg_replace_callback(["/(\?)/"], function() use (&$i, &$arr) {
+		// 	dump($arr);
+		// 	return '"'. $arr[$i++] .'"';
+		// }, $where));
+
+		// // dump(str_replace("/(\?)/", [1,2], $where));
+		// exit;
+
+		$m = new model;
+
+		// $m->where("id=? and name=?", [12, "' or 1=1"]);
+		// $m->table("t_test")->set("ttrr=?", ["wangxian"])->where("id>?", [5])->update();
+
+		// 错误，insert set(array $data) 必须是array类型
+		// $m->table("t_test")->set("name=?, ttrr=?", ["wx", "wangxian"])->insert();
+
+		// 正确
+		// $m->table("t_test")->set(["name" => "wx", "ttrr" => "wangxian"])->insert();
+
+		// query支持查询参数未知替换
+		dump($m->query("select * from t_test where name=?", ["wx"])->findAll());
+
+		dumpdie($m->getLastSql());
+	}
+
 	/** 测试跨model的mysql资源语柄共享 */
 	public function db1Action()
 	{
@@ -27,14 +57,14 @@ class dbController extends controller
 		$t2 = new t2Model();
 		dump($t2->getOne());
 	}
-	
+
 	public function insertAction()
 	{
 		$t1 = new t1Model();
 		dump($t1->add_data('WangXian！@#￥%……&*\'";.,/\''));
 		echo $t1->getLastSql();
 	}
-	
+
 	public function db2Action()
 	{
 		$this->model_t1->getOne();
